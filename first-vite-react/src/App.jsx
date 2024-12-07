45bd811687246f324a5122050d44a730387d4c12
Achinit99 <achinit720@gmail.com>
2024-12-08 01:45:31 +0530

    Arrow Function and Anonymous Function

--- first-vite-react/src/App.jsx
+++ first-vite-react/src/App.jsx
@@ -1,10 +1,31 @@
-import HeaderContent from "./Components/HeaderContent/HeaderContent";
-
 function App() {
+
+  function hello1() {
+    console.log("hello 1");
+  }
+
+  const hello2 = () => {
+    console.log("hello 2");
+  }
+
+  const hello3 = (name) => {
+    console.log("hello 3" + name);
+  }
+
 
   return (
     <>
-      <HeaderContent />
+      <div>
+        <button onClick={hello1}>Click 1</button>
+        <button onClick={hello2}>Click 2</button>
+        <button onClick={
+          () => {
+            hello3("Achini")
+          }
+
+        }>Click 3</button>
+
+      </div>
     </>
   )
 }

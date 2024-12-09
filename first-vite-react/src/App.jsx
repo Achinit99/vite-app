4e9fcf9f3100cd4287662ad36ac5a6018a317866
Achinit99 <achinit720@gmail.com>
2024-12-09 18:44:15 +0530

    delete all files

--- first-vite-react/src/App.jsx
+++ first-vite-react/src/App.jsx
@@ -1,13 +1,7 @@
-import HeaderContent from "./Components/HeaderContent/HeaderContent";
-import BodyContent from "./Components/BodyContent/BodyContent";
-
 function App() {
   return (
     <>
-      <HeaderContent />
-      <BodyContent>
-        <h3>Home page</h3>
-      </BodyContent>
+      <h3> My home page</h3>
     </>
   )
 }

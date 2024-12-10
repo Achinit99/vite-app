6e3bbb17ab4fe7ab03a465095cf43e4b9d6674e0
Achinit99 <achinit720@gmail.com>
2024-12-10 15:35:49 +0530

    Using react router

---
+++ first-vite-react/src/pages/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeaderContent from "./Components/HeaderContent/HeaderContent";
+import BodyContent from "./Components/BodyContent/BodyContent";
+
+function AboutUs() {
+  return (
+    <>
+      <div>
+        <HeaderContent />
+        <BodyContent>
+          <h3>About us page</h3>
+        </BodyContent>
+      </div>
+    </>
+  )
+}
+
+export default AboutUs
---
+++ first-vite-react/src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeaderContent from "./Components/HeaderContent/HeaderContent";
+import BodyContent from "./Components/BodyContent/BodyContent";
+
+function Contact() {
+  return (
+    <div>
+      <div>
+        <HeaderContent />
+        <BodyContent>
+          <h3>Contact page</h3>
+        </BodyContent>
+      </div>
+    </div>
+  )
+}
+
+export default Contact

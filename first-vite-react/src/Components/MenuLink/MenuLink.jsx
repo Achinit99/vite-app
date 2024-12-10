b3ad4e5939fecb18a8160a49d04aa98629c90d3a
Achinit99 <achinit720@gmail.com>
2024-12-10 15:55:57 +0530

    edit menulink file

--- first-vite-react/src/Components/MenuLink/MenuLink.jsx
+++ first-vite-react/src/Components/MenuLink/MenuLink.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 function MenuLink(props) {
   return (
     <>
-      <a href={props.linkurl}>{props.linktext}</a>
+      <Link to={props.linkurl}>{props.linktext}</Link>
     </>
   )
 }
